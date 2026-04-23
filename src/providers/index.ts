5c1a3a08cfecc7c519f21595b630b6628e8b969b
Parenti Michele <mikiparentipc@gmail.com>
2026-04-24 00:43:34 +0200

    feat: add openRouterProvider to manage models from OpenRouter API

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,9 +1,10 @@
 import type { AbstractProvider } from "../types.js";
 import { kiloCodeProvider } from "./provider/kilo.js";
+import { openRouterProvider } from "./provider/openrouter.js";
 
 export { fetchModelsDev } from "./modelsdev.js";
 
 export const activeProviders: AbstractProvider[] = [
-	// Add providers here
 	kiloCodeProvider,
+	openRouterProvider,
 ];
